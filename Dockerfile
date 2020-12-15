fda1700b79a240fa1fa628d4e0bac63a53fd6977
Akila Amarathunga <akamarathunga@deloitte.com.au>
2020-12-15 15:28:53 +1100

    updated docker files

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,37 @@
-FROM alpine:latest
+FROM registry.access.redhat.com/ubi8/python-38
 
-RUN apk add --no-cache python3-dev \
-    && pip3 install --upgrade pip
+LABEL name="FlaskAPI"
+LABEL description="A simple API absed on python flask"
+LABEL maintainer="Akila Amarathunga"
 
-WORKDIR /app
+ARG WORK_DIR=/app
 
-COPY . /app
+ARG PORT
+ARG LOG_LEVEL
+ARG DEBUG
+ARG VERSION
+ARG COMMIT
 
-RUN pip3 --no-cache-dir install -r requirements.txt
+ENV flaskPort=$PORT
+ENV flaskLogLevel=$LOG_LEVEL
+ENV flaskDebug=$DEBUG
+ENV version=$VERSION
+ENV commit_sha=$COMMIT
 
-EXPOSE 5000
+USER root
+
+RUN mkdir ${WORK_DIR}
+
+WORKDIR ${WORK_DIR}
+
+COPY . ${WORK_DIR}
+
+RUN pip3 --no-cache-dir install -r requirements.txt && \
+    chown -R 1001:1001 ${WORK_DIR} 
+
+EXPOSE 8080
+
+USER 1001
 
 ENTRYPOINT ["python3"]
 CMD ["app.py"]
--- app.py
+++ app.py
@@ -4,12 +4,14 @@ import os
 import subprocess
 
 #Environment variables
-port  = int(os.environ.get("flaskPort", 8080))
-level = str(os.environ.get("flaskLogLevel", "INFO"))
-debug = str(os.environ.get("flaskDebug", "True"))
+port       = int(os.environ.get("flaskPort", 8080))
+level      = str(os.environ.get("flaskLogLevel", "INFO"))
+debug      = str(os.environ.get("flaskDebug", "True"))
+version    = os.environ.get("version")
+commit_sha = os.environ.get("commit_sha")
 
-commit_sha = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode("utf-8").strip()
-version    = subprocess.check_output(['git', 'describe', '--tags']).strip().decode("utf-8").split("-")[0].strip()
+# commit_sha = subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode("utf-8").strip()
+# version    = subprocess.check_output(['git', 'describe', '--tags']).strip().decode("utf-8").split("-")[0].strip()
 
 logger.basicConfig(level=level)
 
